78b206b1c0bd9d4bc61b9d82b579f3da5f856e5d
signalrush <signalrush@users.noreply.github.com>
2026-04-19 23:18:51 -0700

    AsymDirectional: widen opposite side to last trade direction (mean-reversion)

--- strategy.sol
+++ strategy.sol
@@ -1,29 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
-
 import {AMMStrategyBase} from "./AMMStrategyBase.sol";
 import {IAMMStrategy, TradeInfo} from "./IAMMStrategy.sol";
-
 contract Strategy is AMMStrategyBase {
     uint256 public constant BASE = 29 * BPS;
-
     function afterInitialize(uint256 ix, uint256 iy) external override returns (uint256, uint256) {
         slots[0] = BASE;
-        slots[1] = wdiv(iy, ix);
+        slots[1] = BASE;
+        slots[2] = wdiv(iy, ix);
         return (BASE, BASE);
     }
-
     function afterSwap(TradeInfo calldata trade) external override returns (uint256, uint256) {
-        uint256 fee = slots[0];
-        uint256 ispot = slots[1];
+        uint256 bidFee = slots[0];
+        uint256 askFee = slots[1];
+        uint256 ispot = slots[2];
         uint256 cspot = wdiv(trade.reserveY, trade.reserveX);
         uint256 xAsY_i = wmul(trade.amountX, ispot);
         uint256 xAsY_c = wmul(trade.amountX, cspot);
         uint256 a = trade.amountY;
         if (xAsY_i > a) a = xAsY_i;
         if (xAsY_c > a) a = xAsY_c;
-        if (trade.isBuy) a = a * 103 / 100;
-        else a = a * 102 / 100;
+        a = a * 102 / 100;
         uint256 r = a / (24 * WAD);
         uint256 bump = r * 36 * BPS;
         if (r == 1) {
@@ -37,17 +34,30 @@ contract Strategy is AMMStrategyBase {
         if (r == 5 && a < 130 * WAD) bump = bump * 31 / 32;
         if (r == 6 && a < 152 * WAD) bump = bump * 63 / 64;
         uint256 target = clampFee(BASE + bump);
-        if (fee < target) {
-            fee = target;
-        } else if (fee > BASE) {
-            uint256 nf = fee * 85 / 100;
-            fee = nf > BASE ? nf : BASE;
+        if (trade.isBuy) {
+            if (askFee < target) askFee = target;
+            else if (askFee > BASE) {
+                uint256 nf = askFee * 85 / 100;
+                askFee = nf > BASE ? nf : BASE;
+            }
+            if (bidFee > BASE) {
+                uint256 nf = bidFee * 85 / 100;
+                bidFee = nf > BASE ? nf : BASE;
+            }
+        } else {
+            if (bidFee < target) bidFee = target;
+            else if (bidFee > BASE) {
+                uint256 nf = bidFee * 85 / 100;
+                bidFee = nf > BASE ? nf : BASE;
+            }
+            if (askFee > BASE) {
+                uint256 nf = askFee * 85 / 100;
+                askFee = nf > BASE ? nf : BASE;
+            }
         }
-        slots[0] = fee;
-        return (fee, fee);
+        slots[0] = bidFee;
+        slots[1] = askFee;
+        return (bidFee, askFee);
     }
-
-    function getName() external pure override returns (string memory) {
-        return "AsymBoost103_102";
-    }
+    function getName() external pure override returns (string memory) { return "AsymDirectional"; }
 }
